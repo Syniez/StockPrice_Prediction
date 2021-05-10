ddedc39df4338c18266e08a1cb52f55ca8452871
Syniez <57395302+Syniez@users.noreply.github.com>
2021-05-10 18:55:40 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,17 +4,37 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import LSTM, Dense
 
-def samsung():
+
+def Data_preprocessing():
     # Load data
     stock = fdr.DataReader('068270', '2000-01-01', '2019-12-31')
-    print(stock.head())
 
-    plt.figure(figsize=(16,9))
+    # Visualization
+    '''plt.figure(figsize=(16,9))
     sns.lineplot(y=stock['Close'], x=stock.index)
     plt.xlabel('time')
     plt.ylabel('price')
-    plt.show()
+    plt.show()'''
+
+    return stock
+
+
+def Model():
+    # Hyper parameters
+    lr = 0.001
+
+    # Build model
+    model = Sequential()
+    model.add(LSTM(16, activation='tanh'))
+    model.add(Dense(16, activatation='relu'))
+
+    optimizer = Adam(learning_rate=lr)
+    model.compile(optimizer=optimizer, loss='Huber', metrics=['mse'])
+    model.fit()
 
 
 if __name__ == '__main__':
